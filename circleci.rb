91c6a18dbb53c3108c7dce8f0f178473255ace9f
EDWARD WEBB <EDWARD.WEBB@LibertyMutual.com>
2018-04-10 09:27:52 -0400

    updated formula to point to local-cli repo

--- circleci.rb
+++ circleci.rb
@@ -1,21 +1,18 @@
 class Circleci < Formula
   desc "Unauthorized mirror of CircleCI CLI for Homebrew"
   homepage "https://edwardawebb.com/projects/creations/homebrew-circleci-cli/"
-  url "https://github.com/eddiewebb/homebrew-circleci/releases/download/0.0.4812-e4f6fcc/circleci-0.0.4812-e4f6fcc.tar.gz"
-  sha256 "18929b848c0ce5e61a4dccd499c73dd4d367b4eacc7532496bd42594ee10357f"
-  
-  depends_on "docker" => :recommended
+  url "https://github.com/eddiewebb/local-cli/releases/download/v0.0.4705-deba4df/circleci-0.0.4705-deba4df.tar.gz"
+  sha256 "2587b22259c9caa70d1354aeaba2c8fa4da7f4dfe394626f932b9c4d2ac2000c"
+  depends_on "docker"
 
   def install
     # Script is standalone bash script
-    mv "circleci.sh","circleci"
+    mv "circleci.sh", "circleci"
     bin.install "circleci"
   end
 
   test do
-    output = shell_output("#{bin}/circleci --version")
-    assert_match "circleci cli version 0.0.4812-e4f6fcc #{version}\n", output
-
-    shell_output "#{bin}/circleci"
+    versionout = shell_output("#{bin}/circleci --version")
+    assert_match "circleci cli version 0.0.4812-e4f6fcc #{version}\n", versionout
   end
 end
